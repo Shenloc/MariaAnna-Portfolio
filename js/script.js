9bc883e9fafcbbd1db658311e064c12e3cda7cc7
Shenloc <pbroniarczyk@outlook.com>
2016-04-21 17:58:23 +0200

    Testimonials & navbar

--- js/script.js
+++ js/script.js
@@ -12,15 +12,7 @@ function init(){
     }
   }
 
-  var navOpen = document.getElementById('open');
-  var navClose = document.getElementById('close');
-
-  /*navOpen.onclick = navToggle;
-
-  function navToggle() {
-    document.getElementById('mobile-nav').setAttribute("style", "tranform:translateY(0)")
-  }*/
-
+  // Mobile nav toggle
   $('#open').on('click', function(){
     $('#mobile-nav').css({
       "transition": ".5s ease",
@@ -30,11 +22,9 @@ function init(){
   $('a').on('click', function(){
     $('#mobile-nav').css({
       "transition": ".5s ease",
-      "transform": "translateY(-100%)"
+      "transform": "translateY(-200%)"
     });
   });
-
-
 
   //video player
   var video = document.getElementById('play-video');
@@ -62,6 +52,67 @@ function init(){
     });
   });
 
+  // smooth scroll
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
+  //slider
+  jQuery(document).ready(function($){
+  	var w,mHeight,tests=$("#testimonials");
+  	w=tests.outerWidth();
+  	mHeight=0;
+  	tests.find(".testimonial").each(function(index){
+  		$("#t_pagers").find(".pager:eq(0)").addClass("active");
+  		if(index==0)
+  			$(this).addClass("active");
+  		if($(this).height()>mHeight)
+  			mHeight=$(this).height();
+  		var l=index*w;
+  		$(this).css("left",l);
+  		tests.find("#test_container").height(mHeight);
+  	});
+
+    $(".pager").on("click",function(e){
+      e.preventDefault();
+      next=$(this).index(".pager");
+      clearInterval(t_int);
+      moveIt(next);
+    });
+
+    var t_int=setInterval(function(){
+      var i=$(".testimonial.active").index(".testimonial");
+      if(i==$(".testimonial").length-1)
+      next=0;
+      else
+      next=i+1;
+      moveIt(next);
+    },3000);
+  });
+
+  function moveIt(next){
+    var c=parseInt($(".testimonial.active").removeClass("active").css("left"));
+    var n=parseInt($(".testimonial").eq(next).addClass("active").css("left"));
+    var view = document.getElementById("testimonials").offsetWidth;
+    $(".testimonial").each(function(){
+      if(n>c)
+      $(this).animate({'left':'-='+view+'px'});
+      else
+      $(this).animate({'left':'+='+Math.abs(n-c)+'px'});
+    });
+    $(".pager.active").removeClass("active");
+    $("#t_pagers").find(".pager").eq(next).addClass("active");
+  }
 
 }
